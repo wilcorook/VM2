a2029943ff0336e38e6a158e7ae6e3d45841ed7f
Wilco Rook <33829948+wilcorook@users.noreply.github.com>
2021-05-23 14:08:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -199,7 +199,7 @@ f_build_inventory() {
 # Function to destroy environment
 f_destroy() {
   (cd "/cloudservice/customers/$1/$2" && vagrant destroy)
-  rm -r "/cloudservice/customers/$1"
+  rm -r "/cloudservice/customers/$1/$2"
 }
 
 # Function to edit environment
